d63a62fe9d8399ab60b5b0bfd7808e52e5852462
ms <altjsvnf@naver.com>
2022-08-05 19:54:18 +0900

    modify: select menu component

--- front/src/pages/repositories/SelectMenu.tsx
+++ front/src/pages/repositories/SelectMenu.tsx
@@ -2,54 +2,30 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useRecoilState, useSetRecoilState } from "recoil";
-import { languageState, pageState, repositoriesState, sortState } from "../../stores/atom";
-import { RepoType } from "../../types/repo";
+import { languageState, pageState, sortState } from "../../stores/atom";
 import { InputLabel } from "@mui/material";
-import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SelectMenu() {
   const [language, setLanguage] = useRecoilState(languageState);
   const [sort, setSort] = useRecoilState(sortState);
-  const setRepos = useSetRecoilState(repositoriesState);
   const setPage = useSetRecoilState(pageState);
-
-  const getRepos = (codeLanguage: string) => {
-    try {
-      if (codeLanguage === "All") return;
-      fetch(`https://api.github.com/orgs/facebook/repos?sort=${sort}`)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error();
-          }
-          return response.json();
-        })
-        .then((data) => {
-          if (codeLanguage === "") return setRepos(data);
-          const filterResult = data.filter((repo: RepoType) => repo.language === codeLanguage);
-          setRepos(filterResult);
-        });
-    } catch (error: any) {
-      console.log(error.message);
-    }
-  };
+  const navigate = useNavigate();
 
   const changeSort = (event: SelectChangeEvent) => {
     setSort(event.target.value);
+    navigate(`?sort=${event.target.value}&language=${language}`);
     setPage(1);
   };
 
   const changeLanguage = (event: SelectChangeEvent) => {
     setLanguage(event?.target.value);
-    getRepos(event?.target.value);
+    navigate(`?sort=${sort}&language=${event?.target.value}`);
     setPage(1);
   };
 
-  useEffect(() => {
-    getRepos(language);
-  }, []);
-
   return (
-    <div>
+    <>
       <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
         <InputLabel id="demo-select-small">Language</InputLabel>
         <Select
@@ -74,7 +50,7 @@ function SelectMenu() {
           <MenuItem value="full_name">이름순</MenuItem>
         </Select>
       </FormControl>
-    </div>
+    </>
   );
 }
 
