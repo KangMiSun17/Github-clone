f0b3f5dc3e99d8ae47c587de6307cbaf6276d912
ms <altjsvnf@naver.com>
2022-08-05 19:53:31 +0900

    modify: overview page

--- front/src/pages/overview/OverView.tsx
+++ front/src/pages/overview/OverView.tsx
@@ -1,17 +1,27 @@
 import { useEffect } from "react";
 import { useResetRecoilState, useSetRecoilState } from "recoil";
-import { languageState, menuValueState, sortState } from "../../stores/atom";
+import {
+  languageState,
+  menuValueState,
+  pageState,
+  searchWordState,
+  sortState,
+} from "../../stores/atom";
 import { Content } from "../../styles/menu";
 
 function OverView() {
   const resetLanguage = useResetRecoilState(languageState);
   const resetSort = useResetRecoilState(sortState);
   const setMenuValue = useSetRecoilState(menuValueState);
+  const setSearchWord = useSetRecoilState(searchWordState);
+  const setPage = useSetRecoilState(pageState);
 
   useEffect(() => {
     resetLanguage();
     resetSort();
     setMenuValue(0);
+    setSearchWord("");
+    setPage(1);
   }, []);
 
   return <Content>OverView 페이지입니다.</Content>;
